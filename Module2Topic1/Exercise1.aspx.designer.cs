ee629660382a9a8e33a2ede4ebcaa4858506f983
Neoron Lopez <neoronlopezcxz@gmail.com>
2024-02-27 01:01:42 +0800

    Activity

--- Module2Topic1/Exercise1.aspx.designer.cs
+++ Module2Topic1/Exercise1.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Module1Exercise1
 
     public partial class Exercise1
     {
+
+        /// <summary>
+        /// lblFavoriteSong control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFavoriteSong;
     }
 }
